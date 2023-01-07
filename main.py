922bd9828cd1924223dad95256420a0ecc7474f6
Kevin Nguyen <thenoc27@gmail.com>
2023-01-07 10:57:13 -0500

    Main functionality solved

---
+++ main.py
@@ -0,0 +1,82 @@
+import turtle
+# 400 by 300 unit screen with wn.screensize()
+# Sets direction and cartesian positions for next turtles
+def style_turtle(t, x, y):
+  t.hideturtle()
+  t.setheading(60)
+  t.penup()
+  t.setpos(x,y)
+  t.pendown()
+  t.speed(0)
+  t.color('white')
+  return t
+
+def drawMain(t, l):
+  t.left(60)
+  t.forward(l)
+  t.right(120)
+  t.forward(l)
+  t.right(120)
+  t.forward(l)
+
+def drawInverted(t, l):
+  # given a triangle any triangle
+  # draw an inverted triangle within it
+  # to get the inverted triangle within others you must use recursion
+  # I idea I have is to create 3 turtles at 3 key points to continue the drawing. Since recursion we should set a limit
+  # Until the recursion stack stops, this base case will be when the turtles will stop
+  if (l <= 5):
+    return
+  else:
+    # set turtle directions in the same directions
+    # Set the positions of the next turtles at key places
+    # Place bottom left turtle here
+    a = turtle.Turtle()
+    a = style_turtle(a, t.xcor(), t.ycor())
+    t.forward(l)
+    
+    #Place top turtle here
+    b = turtle.Turtle()
+    b = style_turtle(b, t.xcor(), t.ycor())
+    t.right(60)
+    t.forward(l)
+    t.right(120)
+    t.forward(l)
+    
+    #Place bottom right turtle
+    c = turtle.Turtle()
+    c = style_turtle(c, t.xcor(), t.ycor())
+    t.right(120)
+    t.forward(l)
+    
+    # Probably have to finished the drawing, and then multiplication can begin; though test first
+    # Hides current turtle and gives show to the next turtles
+    # Start stack for bottom left
+    drawInverted(a, l/2)
+    drawInverted(b, l/2)
+    drawInverted(c, l/2)
+    return
+
+def main():
+  # Turtle setup  
+  wn = turtle.Screen()
+  wn.bgcolor('black')
+  length = 700
+  wn.setup(length * 2, length * 1.5)
+  # Sets the speed
+  wn.delay(0)
+  alyx = turtle.Turtle()
+  alyx.color('white')
+  alyx.hideturtle
+  # Turtle positioning
+  alyx.penup()
+  alyx.setpos(-length / 2, -length / 2.5)
+  alyx.pendown()
+
+  # Draws the main triangle and positions turtle
+  drawMain(alyx, length)
+  alyx.setheading(60)
+  
+  drawInverted(alyx, length/2)
+  wn.exitonclick()
+main()
